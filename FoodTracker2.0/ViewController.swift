f248fd21298f3e9d4d2e0b37d3a0c62e4db18d07
Rollin Francois <Rollin@rollins-macbook-pro.local>
2016-08-17 17:57:16 -0400

    Create a label reset action in the ViewController.swift code.

--- FoodTracker2.0/ViewController.swift
+++ FoodTracker2.0/ViewController.swift
@@ -19,6 +19,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // ROLLIN: Action
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+    }
+    
 
 }
 
