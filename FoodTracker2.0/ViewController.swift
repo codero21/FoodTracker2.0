41bd2a7f7eb7a625b3f6a54c24219b3496ece6d2
Rollin Francois <Rollin@rollins-macbook-pro.local>
2016-08-17 11:45:10 -0400

    Connect the text field to the ViewCOntroller.swift code

--- FoodTracker2.0/ViewController.swift
+++ FoodTracker2.0/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // ROLLIN: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
