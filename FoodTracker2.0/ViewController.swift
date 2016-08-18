a2069538b6093193f91a33e908e1bc2dfa874ad9
Rollin Francois <Rollin@rollins-macbook-pro.local>
2016-08-17 22:00:18 -0400

    Adopt the UITextFieldDelegate protocol.

--- FoodTracker2.0/ViewController.swift
+++ FoodTracker2.0/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     // ROLLIN: Properties
     @IBOutlet weak var nameTextField: UITextField!
