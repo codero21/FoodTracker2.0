d49ddbe61af77a5693b2d6e6cb45e247072deee0
Rollin Francois <Rollin@rollins-macbook-pro.local>
2016-08-19 23:53:38 -0400

    Create a subclass of UIView and override the initializer

---
+++ FoodTracker2.0/RatingControl.swift
@@ -0,0 +1,18 @@
+//
+//  RatingControl.swift
+//  FoodTracker2.0
+//
+//  Created by Rollin Francois on 8/19/16.
+//  Copyright © 2016 Rollin Francois. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    // ROLLIN: Initializer
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
