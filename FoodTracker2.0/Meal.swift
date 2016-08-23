9d4824db91bd482d746bc4f266f08a4fae08ca1d
Rollin Francois <Rollin@rollins-macbook-pro.local>
2016-08-22 22:57:29 -0400

    Create a new data model

---
+++ FoodTracker2.0/Meal.swift
@@ -0,0 +1,21 @@
+//
+//  Meal.swift
+//  FoodTracker2.0
+//
+//  Created by Rollin Francois on 8/22/16.
+//  Copyright © 2016 Rollin Francois. All rights reserved.
+//
+
+import UIKit
+
+class Meal: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
