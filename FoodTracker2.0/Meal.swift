263ab2abe23c573418a5c44e512e5e5b0be23ec9
Rollin Francois <Rollin@rollins-macbook-pro.local>
2016-08-23 13:20:06 -0400

    Create a new data model class

--- FoodTracker2.0/Meal.swift
+++ FoodTracker2.0/Meal.swift
@@ -2,20 +2,29 @@
 //  Meal.swift
 //  FoodTracker2.0
 //
-//  Created by Rollin Francois on 8/22/16.
+//  Created by Rollin Francois on 8/23/16.
 //  Copyright © 2016 Rollin Francois. All rights reserved.
 //
 
 import UIKit
 
-class Meal: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+class Meal {
+    
+    // ROLLIN: Properties
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialize stored properties
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Initialization should fail if there is no name of if the rating is negative
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
     }
-    */
-
 }
